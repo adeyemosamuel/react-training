d9c19443b036a1fcd4d18761d919a77e0131b78a
Macbook <macbook@Macbooks-MBP-2.domain.name>
2018-07-02 16:58:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+// import App from './App';
+// import MainApp from './src/MainApp';
+import MusicPlayer from './src/MusicPlayer';
+
+AppRegistry.registerComponent('tutorial', () => MusicPlayer);
