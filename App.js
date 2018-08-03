9e00323d62e2b13ad3952829b01cef159fa29aaf
adeyemosamuel <adeyemosamuel@gmail.com>
2018-08-03 15:06:41 +0100

    update

--- App.js
+++ App.js
@@ -1,30 +1,66 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { createDrawerNavigator } from "react-navigation";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  Dimensions,
+  Image
+} from "react-native";
+
+import { createDrawerNavigator, DrawerItems } from "react-navigation";
 import HomeScreen from "./screens/HomeScreen";
 import SettingsScreen from "./screens/SettingsScreen";
 
 
+const { width } = Dimensions.get('window')
 export default class App extends React.Component {
   render() {
-    return (
-    <AppDrawerNavigator />
-    );
+    return <AppDrawerNavigator />;
   }
 }
 
+const CustomDrawerComponent = props => (
+  <SafeAreaView style={{ flex: 1 }}>
+    <View
+      style={{
+        height: 150,
+        backgroundColor: "white",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      <Image
+        source={require("./src/images/sam.jpg")}
+        style={{ height: 120, width: 120, borderRadius: 60 }}
+      />
+    </View>
+    <ScrollView>
+      <DrawerItems {...props} />
+    </ScrollView>
+  </SafeAreaView>
+);
 
-const AppDrawerNavigator = createDrawerNavigator({
-  Home: HomeScreen,
-  Settings: SettingsScreen
-})
+const AppDrawerNavigator = createDrawerNavigator(
+  {
+    Home: HomeScreen,
+    Settings: SettingsScreen
+  },
+  {
+    contentComponent: CustomDrawerComponent,
+    drawerWidth: width,
+    contentOptions:{
+      activeTintColor: 'orange'
+    }
+  }
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF" 
+    backgroundColor: "#F5FCFF"
   }
-  
 });
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Header, Left, Right, Icon} from 'native-base';
 
 class HomeScreen extends Component {
+
+  static navigationOptions= {
+    drawerIcon: ({ tintColor })=>(
+      <Icon name="home" style={{fontSize: 24, color: tintColor}}/>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
+      <Header>
+        <Left>
+          <Icon name="menu" onPress={()=>this.props.navigation.openDrawer()}/>
+        </Left>
+      </Header>
+      <View style={{flex: 1, alignItems:'center',justifyContent:'center'}}>
         <Text>HomeScreen</Text>
+        </View>
       </View>
     );
   }
@@ -16,7 +31,6 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
+
   }
 });
